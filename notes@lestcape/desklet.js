ab6844eec7dd37009730990f29b7986a7a7380c9
lestcape <lestcape@gmail.com>
2013-11-06 19:32:12 -0600

    Inittial Release.

--- notes@lestcape/desklet.js
+++ notes@lestcape/desklet.js
@@ -1,13 +1,13 @@
 
-// Time And Date Cinnamon Desklet v0.1 - 19 June 2013
+// Notes Cinnamon Desklet v0.1 - 6 November 2013
+
+//This is a simple desklet to add notes in the desktop.
+//This is only for experimental uses.
+//Base on https://github.com/linuxmint/Cinnamon/pull/2119
+//of dalcde https://github.com/dalcde
 //
-// This is a simple desklet to display the time and date. The size and format of the date are configurable by changing the values in metadata.json. 
-// This can be launched from the Desklet itself by selecting Config from the menu.
-// 
-// I'm sharing it in case it useful to anyone else especially as there do not seem to be many Cinammon Desklets yet. 
-//
-// -Steve
-// desklets [at] stargw [dot] eu
+// Lester Carballo Pérez
+// lestcape@gmail.com
 
 const Gio = imports.gi.Gio;
 const St = imports.gi.St;
